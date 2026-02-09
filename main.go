538ce29ad5152112c2f72cb411c5c4a90eaabf6e
M YAMINI SARASWATHI <myaminisaraswathi@Ms-MacBook-Air.local>
2026-02-09 22:44:34 +0530

    COmpeted get and post

--- main.go
+++ main.go
@@ -1,28 +1,49 @@
 package main
-import "fmt"
-// import "net/http"
-import "github.com/gin-gonic/gin"
-import "RestApiProject/models"
+
+import (
+	"fmt"
+	"net/http"
+	"RestApiProject/models"
+	"time"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 	fmt.Println("Hello, World!")
-	server :=gin.Default() // It will setup an engine for us and return it. The default engine is a Logger and Recovery middleware already attached.
+	server := gin.Default() // It will setup an engine for us and return it. The default engine is a Logger and Recovery middleware already attached.
 	// Handler for GET request to the root path
-	server.GET("/blog",getBlog) // This is an endpoint for GET request 
+	server.GET("/blog", getBlog) // This is an endpoint for GET request
+	server.POST("/blog", createBlog)
+	// server.GET("/blog", getBlog)
 	server.Run(":8080") // listen and serve on
 }
 
 // In this gin.Context already has request and response objects
-func getBlog(context *gin.Context){ //context parameter will be set by gin and will be pointer to gin
+func getBlog(context *gin.Context) { //context parameter will be set by gin and will be pointer to gin
 	// Reach out to a database
 	// Write in a file
 	// Make an API call to another service
-	context.JSON(200,gin.H{
-			"ID":        "245",
-			"Author":    "Yamini",
-			"Title":     "This is a post request",
-			"Content":   "This is the content of the post request",
-			"CreatedAt": "2024-06-01T12:00:00Z",
-			"UpdatedAt": "2024-06-01T12:00:00Z",
-	}) // To send the response
-// gin.H is a shortcut for map[string]interface{} which is a map with string keys and values
+	blogs := models.GetAllBlogs()
+	context.JSON(http.StatusOK, blogs) // To send the response
+	// gin.H is a shortcut for map[string]interface{} which is a map with string keys and values
+}
+func createBlog(context *gin.Context) {
+	var blog models.Blog
+	err := context.ShouldBindJSON(&blog) // This will mostly work as scan functions
+	// we need to pass a pointer to the blog to modify data in the blog variable
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "couldnt parse request",
+			"error":   err.Error(),
+		})
+		return
+	}
+	blog.ID = len(models.GetAllBlogs()) + 1
+
+	blog.CreatedAt = time.Now()
+	blog.UpdatedAt = time.Now()
+
+	blog.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Blog created successfully", "blog": blog})
+
 }
--- models/blogs.go
+++ models/blogs.go
@@ -4,21 +4,23 @@ package models
 import "time"
 
 type Blog struct {
-	ID        int
-	Author    string
-	Title     string
-	Content   string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `json:"id"`
+	Author    string    `json:"author"` // struct tags we need to mention for the one which is important
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
-var blogs =[] Blog {}
-func (e Blog) Save(){
+var blogs = []Blog{}
+
+func (e Blog) Save() {
 	// store in json file later
-	blogs = append(blogs,e)
+	blogs = append(blogs, e)
 }
+
 // Its just a function not a method
-func GetAllBlogs() [] Blog{
+func GetAllBlogs() []Blog {
 	return blogs
 
 }
